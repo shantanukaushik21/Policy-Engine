5cc1b2deb3074d0ed0f96e2bde947e237edd7670
Shantanu Kaushik <shantanu.kaushik21@gmail.com>
2025-10-02 23:28:33 -0500

    Initial Policy class and condition groups

--- Policy Engine/src/main/java/com/PolicyEngine/demo/model/Condition.java
+++ Policy Engine/src/main/java/com/PolicyEngine/demo/model/Condition.java
@@ -1,4 +1,7 @@
 package com.PolicyEngine.demo.model;
 
 public class Condition {
+    private String field;    // e.g. "creditScore"
+    private String operator; // ==, !=, >=, <=, <, >
+    private Object value;    // flexible type (string, int, double, boolean)
 }
